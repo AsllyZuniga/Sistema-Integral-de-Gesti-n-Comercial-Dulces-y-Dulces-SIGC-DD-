bbe3f02a4ca897ccb77c3dec7c4707ffed0adac7
Felipe Rivas <79724739+feliperivasdev@users.noreply.github.com>
2026-02-13 08:51:31 -0500

    fix: Ajustes Css login

--- src/login/login.ts
+++ src/login/login.ts
@@ -3,30 +3,29 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 @Component({
-selector: 'app-login',
-standalone: true,
-templateUrl: './login.html',
-styleUrls: ['./login.css'],
-imports: [FormsModule, CommonModule]
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.html',
+  styleUrls: ['./login.css'],
+  imports: [FormsModule, CommonModule],
 })
 export class LoginComponent implements OnInit {
-is_error = false;
-user = {
-usr: "",
-pass: ""
-};
-constructor(
-private router: Router,
-) { }
-ngOnInit() {
-}
-validarUsuario() {
-if (this.user.usr == 'admin' && this.user.pass == '123') {
-this.is_error = false;
-this.router.navigate(['/home']);
-}
-else {
-this.is_error = true;
-}
-}
+  is_error = false;
+  user = {
+    usr: '',
+    pass: '',
+  };
+  constructor(private router: Router) {}
+  ngOnInit() {}
+  validarUsuario() {
+    if (this.user.usr == 'admin' && this.user.pass == '1234') {
+      this.is_error = false;
+      this.router.navigate(['/home']);
+    } else {
+      this.is_error = true;
+    }
+  }
+  goToAdmin() {
+    this.router.navigate(['/supervisor']);
+  }
 }
