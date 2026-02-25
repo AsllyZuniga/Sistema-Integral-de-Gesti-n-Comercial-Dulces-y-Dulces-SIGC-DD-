023112329bb8cd35148425e548dcb347694b826a
Aslly Zuñiga <asllyz2610@gmail.com>
2026-02-25 12:02:42 -0500

    Se actualiza servicio

--- src/app/core/services/ventas/cumplimientoVentasMes.service.ts
+++ src/app/core/services/ventas/cumplimientoVentasMes.service.ts
@@ -3,50 +3,69 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map, catchError, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CumplimientoService {
   private apiUrl = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getCumplimientoMes(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/mes/cumplimiento`).pipe(
-      map(res => Array.isArray(res) ? res : []),
-      catchError(() => of([]))
+      map((res) => (Array.isArray(res) ? res : [])),
+      catchError(() => of([])),
     );
   }
 
   getCumplimientoPorCodigo(codigo: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/mes/cumplimiento/${codigo}`).pipe(
-      catchError(() => of(null))
-    );
+    return this.http
+      .get<any>(`${this.apiUrl}/mes/cumplimiento/${codigo}`)
+      .pipe(catchError(() => of(null)));
   }
 
   getLineasPorVendedor(codigoVendedor: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/mes/cumplimiento/vendedor/${codigoVendedor}/lineas`).pipe(
-      map(res => {
-        // Aseguramos que la propiedad detallePorLinea siempre sea un array
-        if (res && res.detallePorLinea) {
-          res.detallePorLinea = Array.isArray(res.detallePorLinea) ? res.detallePorLinea : [];
-        }
-        return res;
-      }),
-      catchError(() => of({ detallePorLinea: [] }))
-    );
+    return this.http
+      .get<any>(`${this.apiUrl}/mes/cumplimiento/vendedor/${codigoVendedor}/lineas`)
+      .pipe(
+        map((res) => {
+          // Aseguramos que la propiedad detallePorLinea siempre sea un array
+          if (res && res.detallePorLinea) {
+            res.detallePorLinea = Array.isArray(res.detallePorLinea) ? res.detallePorLinea : [];
+          }
+          return res;
+        }),
+        catchError(() => of({ detallePorLinea: [] })),
+      );
   }
 
   getCiudadesPorVendedor(codigoVendedor: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/mes/cumplimiento/vendedor/${codigoVendedor}/ciudades`).pipe(
-      map(res => {
-        // Aseguramos que la propiedad detallePorCiudad siempre sea un array
-        if (res && res.detallePorCiudad) {
-          res.detallePorCiudad = Array.isArray(res.detallePorCiudad) ? res.detallePorCiudad : [];
-        }
-        return res;
-      }),
-      catchError(() => of({ detallePorCiudad: [] }))
-    );
+    return this.http
+      .get<any>(`${this.apiUrl}/mes/cumplimiento/vendedor/${codigoVendedor}/ciudades`)
+      .pipe(
+        map((res) => {
+          // Aseguramos que la propiedad detallePorCiudad siempre sea un array
+          if (res && res.detallePorCiudad) {
+            res.detallePorCiudad = Array.isArray(res.detallePorCiudad) ? res.detallePorCiudad : [];
+          }
+          return res;
+        }),
+        catchError(() => of({ detallePorCiudad: [] })),
+      );
   }
-
+  // NUEVO ENDPOINT → DETALLE POR ITEM (PRODUCTOS)
+  getProductosPorVendedor(codigoVendedor: string): Observable<any> {
+    return this.http
+      .get<any>(`${this.apiUrl}/mes/cumplimiento/vendedor/${codigoVendedor}/productos`)
+      .pipe(
+        map((res) => {
+          if (res && res.detallePorProducto) {
+            res.detallePorProducto = Array.isArray(res.detallePorProducto)
+              ? res.detallePorProducto
+              : [];
+          }
+          return res;
+        }),
+        catchError(() => of({ detallePorProducto: [] })),
+      );
+  }
 }
